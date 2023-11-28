2fbe3f4605dbfa542237d8278853bd343065be5b
Jomar Banting <shadowlover.05@gmail.com>
2023-11-27 19:25:29 -0500

    reinstalling modules for codegrade

--- api/server.js
+++ api/server.js
@@ -1,7 +1,3 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -14,11 +10,13 @@ const cors = require("cors");
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
  */
-const session = require("express-session");
-const Store = require("connect-session-knex")(session);
-const userRouter = require("./users/users-router");
-const authRouter = require("./auth/auth-router");
-
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const userRouter = require("./users/users-router")
+const authRouter = require("./auth/auth-router")
+const session = require("express-session")
+const Store = require("connect-session-knex")(session)
 
 const server = express();
 
@@ -29,21 +27,20 @@ server.use(cors());
 server.use(session({
   name: "chocolatechip",
   secret: `keep it secret`,
-  cookie: {
-    maxAge: 1000 * 60 * 60,
-    secure: false,
-    httpOnly: false,
-  },
-  rolling: true,
-  resave: false,
   saveUninitialized: false,
+  resave: false,
   store: new Store({
     knex: require("../data/db-config"),
     tablename: "sessions",
     sidfieldname: "sid",
     createtable: true,
-    clearInterval: 1000 * 60 * 60
-  })
+    clearInterval: 1000 * 60 * 10
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  },
 }))
 
 server.use("/api/users", userRouter);
